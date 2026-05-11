9153a95cdaf1594c1a1ea838314b8822baa4444e
maitatduy <maitatduy.work@gmail.com>
2026-05-11 21:20:59 +0700

    chore(backend): setup mongodb connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,30 @@
-console.log("Music Resource Platform");
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+import "dotenv/config";
+
+import express from "express";
+import databaseService from "~/services/database.service";
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+async function startServer() {
+    try {
+        await databaseService.connect();
+
+        console.log("Đã kết nối database thành công!");
+
+        app.listen(PORT, () => {
+            console.log(`Server đang chạy ở cổng ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Kết nối database thất bại!");
+        console.log(error);
+    }
+}
+
+startServer();
---
+++ backend/src/services/database.service.ts
@@ -0,0 +1,32 @@
+import { MongoClient, Db } from "mongodb";
+
+const uri = process.env.MONGODB_URI as string;
+
+class DatabaseService {
+    private client: MongoClient;
+    private db: Db;
+
+    constructor() {
+        this.client = new MongoClient(uri);
+        this.db = this.client.db("music-resource-platform");
+    }
+
+    async connect() {
+        try {
+            await this.client.connect();
+
+            await this.db.command({
+                ping: 1,
+            });
+
+            console.log("Kết nối MongoDB thành công!");
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+}
+
+const databaseService = new DatabaseService();
+
+export default databaseService;
