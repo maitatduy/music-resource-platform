7c22d713234ed69ebbbdbe99f01389f011cb066b
maitatduy <maitatduy.work@gmail.com>
2026-05-12 21:22:48 +0700

    chore(frontend): initialize frontend project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button.tsx";
+
+export default function App() {
+    return (
+        <>
+            <div>Music Resource Platform</div>
+            <Button>Đăng ký</Button>
+            <Button>Đăng nhập</Button>
+        </>
+    );
+}
